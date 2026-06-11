4f4a3a90b595293590d92755f92293d6c8721840
azrilmustofa <m.azrilmustofa@gmail.com>
2026-06-11 20:42:54 +0700

    fix: add jQuery and DataTables CDN to admin layout

--- resources/views/layouts/master_admin.blade.php
+++ resources/views/layouts/master_admin.blade.php
@@ -14,6 +14,7 @@
     <link href="{{ asset('template_admin/css/style.css') }}" rel="stylesheet">
     <link rel="icon" type="image/png" href="{{ asset('template_customer/images/erasebg.png') }}?v=1">
 	<link rel="shortcut icon" type="image/png" href="{{ asset('template_customer/images/erasebg.png') }}?v=1">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
 </head>
 
 <body>
@@ -30,9 +31,12 @@
     </div>
 
     {{-- JS --}}
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="{{ asset('template_admin/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('template_admin/js/tiny-slider.js') }}"></script>
     <script src="{{ asset('template_admin/js/custom.js') }}"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
     <script>
         $(document).ready(function () {
             $('#myTable').DataTable();
