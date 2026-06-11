2351b9035268951335f3a997ea37b87b3ca59989
azrilmustofa <m.azrilmustofa@gmail.com>
2026-06-11 20:08:19 +0700

    fix: trust all proxies so Railway HTTPS generates correct asset URLs

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware): void {
 
+        $middleware->trustProxies(at: '*');
+
+
         $middleware->group('admin', [
             \Illuminate\Auth\Middleware\Authenticate::class,
             \App\Http\Middleware\admin::class,
